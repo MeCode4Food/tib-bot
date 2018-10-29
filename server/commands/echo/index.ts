dc235a4e0c117d6bf25499b01fb6ace39d843f54
CK L <cheekhuin.loong@gmail.com>
2018-10-29 10:49:32 +0800

    attempt to add linter to the project
    fixed EchoCommand wrt linting

--- server/commands/echo/index.ts
+++ server/commands/echo/index.ts
@@ -1,18 +1,18 @@
-import { Message } from 'discord.js'
-import Command from '../_command'
-import { DiscordBot } from '../../discord-bot';
+import { Message } from "discord.js";
+import { DiscordBot } from "../../discord-bot";
+import Command from "../_command";
 
-export default class EchoCommand extends Command{
+export default class EchoCommand extends Command {
 
-    constructor(){
-        let name = 'echo'
-        let description = 'echoes the message by the sender'
-        super(name, description)
+    constructor() {
+        super();
+        this.name = "echo";
+        this.description = "echoes the message by the sender";
     }
 
-    public execute(discordBot: DiscordBot, message: Message, args: string[]){
-        const reducer = (accumulator: string, currentValue: string) => accumulator + currentValue
-        const messageContent = args.reduce(reducer)
-        message.channel.send(messageContent)
+    public execute(discordBot: DiscordBot, message: Message, args: string[]): void {
+        const reducer = (accumulator: string, currentValue: string) => accumulator + currentValue;
+        const messageContent = args.reduce(reducer);
+        message.channel.send(messageContent);
     }
 }
