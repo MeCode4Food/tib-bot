3650f8fb36255f3af6e3f366d98b1b6183838558
CK L <cheekhuin.loong@gmail.com>
2018-11-16 13:57:04 +0800

    added helper folder for functions related to cards
    did refactoring for cards

---
+++ server/commands/card/helper/index.ts
@@ -0,0 +1,37 @@
+import { RichEmbed } from "discord.js";
+import axios from "axios";
+
+export async function getCardfromUrl(dbUrl: string): Promise<any> {
+  return (await axios.get(dbUrl)).data.data;
+}
+
+export function getDBUrl(cardQuery: string): string {
+  let dbUrl = "http://";
+  dbUrl += process.env.DB_API_URL ? process.env.DB_API_URL : "127.0.0.1";
+  dbUrl += `:${process.env.DB_API_PORT}/card?query=${cardQuery}`;
+
+  return dbUrl;
+}
+
+export function generateEmbedFromCard(card: any): RichEmbed {
+  const embed: RichEmbed = new RichEmbed()
+                .setColor(4868682)
+                .addField("Card Name", card.card_name, true)
+                .addField("Card Type", card.card_type, true);
+
+  if (card.rarity) { embed.addField("Rarity", card.rarity); }
+  if (card.attack || card.armour || card.hit_points ) {
+      embed.addField("Stats", `${card.attack || 0 }/${card.armour || 0}/${card.hit_points || 0}`, true);
+  }
+  // if (card.attack) { embed.addField("Attack", card.attack, true); }
+  // if (card.armour) { embed.addField("Armor", card.armour, true); }
+  // if (card.hit_points) { embed.addField("Health", card.hit_points, true); }
+  if (card.mana_cost) { embed.addField("Mana Cost", card.mana_cost, true); }
+  if (card.card_text) { embed.addField("Card Text", card.card_text); }
+  if (card.signature_name) { embed.addField("Signature Card", card.signature_name); }
+  if (card.passive_name) { embed.addField("Passive Ability", `**${card.passive_name}**: ${card.passive_text}`); }
+  if (card.parent_type === "Hero") { embed.addField("Signature Card for ", `${card.parent_name}`, true); }
+  if (card.card_image) { embed.setThumbnail(card.card_image); }
+
+  return embed;
+}
--- server/commands/card/index.ts
+++ server/commands/card/index.ts
@@ -1,7 +1,8 @@
 import { Message, RichEmbed } from "discord.js";
 import { DiscordBot } from "../../discord-bot";
+import { getDBUrl, generateEmbedFromCard, getCardfromUrl } from "./helper";
 import Command from "../_command";
-import axios from "axios";
+import _ from "lodash";
 
 export default class CardCommand extends Command {
     public name: string;
@@ -15,32 +16,21 @@ export default class CardCommand extends Command {
 
     public async execute(discordBot: DiscordBot, message: Message, args: string[]): Promise<void> {
         const cardQuery = args.join(" ");
-        let dbUrl = "http://";
-        dbUrl += process.env.DB_API_URL ? process.env.DB_API_URL : "127.0.0.1";
-        dbUrl += `:${process.env.DB_API_PORT}/card?query=${cardQuery}`;
 
-        const response: any = (await axios.get(dbUrl)).data.data;
+        // construct db url from card query
+        const dbUrl = getDBUrl(cardQuery);
 
-        if (response.length === 0) {
+        // obtain results from db
+        const response: any = getCardfromUrl(dbUrl);
+
+        // if response is empty, return error
+        if (_.isEmpty(response)) {
             message.channel.send(`Card '${cardQuery}' not found`);
         } else {
-            const embed: RichEmbed = new RichEmbed()
-                .setColor(4868682)
-                .addField("Card Name", response.card_name, true)
-                .addField("Card Type", response.card_type, true);
 
-            if (response.rarity) { embed.addField("Rarity", response.rarity); }
-            if (response.attack) { embed.addField("Attack", response.attack, true); }
-            if (response.armour) { embed.addField("Armor", response.armour, true); }
-            if (response.hit_points) { embed.addField("Hit Points", response.hit_points, true); }
-            if (response.mana_cost) { embed.addField("Mana Cost", response.mana_cost, true); }
-            if (response.card_text) { embed.addField("Card Text", response.card_text); }
-            if (response.signature_name) { embed.addField("Signature Card", response.signature_name); }
-            if (response.passive_name) { embed.addField("Passive Ability", `${response.passive_name} ${response.passive_text}`); }
-            if (response.parent_type === "Hero") { embed.addField("Signature Card for ", `${response.parent_name}`, true); }
-            if (response.card_image) { embed.setThumbnail(response.card_image); }
+            // using the card response, generate the embed
+            const embed = generateEmbedFromCard(response);
 
-            console.log(response);
             message.channel.send(embed);
         }
     }
