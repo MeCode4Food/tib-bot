b1ca511f5c995d987b80ec1691e620090353f353
CK L <cheekhuin.loong@gmail.com>
2018-12-05 08:34:25 +0800

    added deck code decoding for deck codes with playartifact url in them

--- server/discord-bot.ts
+++ server/discord-bot.ts
@@ -6,6 +6,7 @@ import chalk from "chalk";
 import SIGNALE from "signale";
 import * as _ from "lodash";
 import fs from "fs";
+import { isDeckCodeInMessage } from "./message_handlers/deck_code_handler/helper/is_deck_code_in_message";
 
 export class DiscordBot {
     private client = new Discord.Client();
@@ -124,7 +125,7 @@ export class DiscordBot {
                     console.log(error);
                 }
             }
-        } else if (message.content.startsWith(this.deckCodePrefix!)) {
+        } else if (isDeckCodeInMessage(message)) {
             // decode deck code
             handleDeckCodeMessage(message);
         }
