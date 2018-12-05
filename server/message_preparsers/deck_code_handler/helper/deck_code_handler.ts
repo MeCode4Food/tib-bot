14bf54222d5b92e892359a02e856713ed3be0330
CK L <cheekhuin.loong@gmail.com>
2018-12-06 07:15:20 +0800

    Revert "WIP deck code"
    This reverts commit 3e14a0a4de38b7c568190ca0525b8988fb477a3d.

--- server/message_preparsers/deck_code_handler/helper/deck_code_handler.ts
+++ server/message_preparsers/deck_code_handler/helper/deck_code_handler.ts
@@ -7,46 +7,29 @@ export default class DeckCodeHandler {
 
   // verifies checksum for deck code. takes in deck code with or without prefix
   public static verifyCheckSum(deckCodeWithPrefix: string) {
-    const currentVersion = 2;
-    console.log(process.env.DECK_CODE_PREFIX);
     const deckCode = deckCodeWithPrefix.startsWith(process.env.DECK_CODE_PREFIX!) ? this.trimAndFixDeckCode(deckCodeWithPrefix) : deckCodeWithPrefix;
-    console.log(deckCode);
+
     const deckByteArray: number[] = [];
 
     // Bufferize deckCode to convert to array
-    const decoded = Buffer.from(deckCode, "base64").toString("binary");
+    Buffer.from(deckCode, "base64").map((e) => deckByteArray.push(e));
 
-    for (let i = 0; i < decoded.length; i++) {
-      deckByteArray.push(decoded.charCodeAt(i));
-    }
-
-    console.log(JSON.stringify(deckByteArray));
-    console.log(JSON.stringify(Buffer.from(deckCode, "base64").toString("binary")));
-
-    const $nCurrentByteIndex = [0];
-    const $nTotalBytes = deckByteArray.length;
-    // check version num
-    const $nVersionAndHeroes = deckByteArray[$nCurrentByteIndex[0]++];
-    const $version = $nVersionAndHeroes >> 4;
-
-    if (currentVersion !== $version && $version !== 1) { return false; }
-
-    // do checksum check
-    const $nChecksum = deckByteArray[$nCurrentByteIndex[0]++];
-    let $nStringLength = 0;
-    if ($version > 1) { $nStringLength = deckByteArray[$nCurrentByteIndex[0]++]; }
-    const $nTotalCardBytes = $nTotalBytes - $nStringLength;
+    const version = deckByteArray[0] >> 4;
+    const checkSum = deckByteArray[1];
+    let stringLength = 0;
+    if (version > 1) { stringLength = deckByteArray[2]; }
+    const totalCardBytes = deckByteArray.length - stringLength;
     // grab the string size
-    // {
-    let $nComputedChecksum = 0;
-    for (let $i = $nCurrentByteIndex[0]; $i < $nTotalCardBytes; $i++) { $nComputedChecksum += deckByteArray[$i]; }
-    const $masked = ($nComputedChecksum & 0xFF);
-    console.log(calcCheckSum);
+
+    let calcCheckSum = 0;
+    for (let i = 3; i < totalCardBytes; i++) { calcCheckSum += deckByteArray[i]; }
+    calcCheckSum = (calcCheckSum & 0xFF);
+
     // comapre checksums
     return checkSum === calcCheckSum ? true : false;
   }
 
-  public static trimAndFixDeckCode(deckCodeWithPrefix: string): string { return deckCodeWithPrefix.replace(/-/g, "/").replace(/_/g, "=").slice(3); }
+  public static trimAndFixDeckCode(deckCodeWithPrefix: string): string { return deckCodeWithPrefix.replace("-", "/").replace("_", "=").slice(3); }
 
   public static async requestDeckObject(deckCodeWithPrefix: string) {
     const dbUrl = getDBUrl(deckCodeWithPrefix);
