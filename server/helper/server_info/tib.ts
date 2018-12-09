5e1b4f4e1825989ad04678f04a608f08b2d20d0a
CK L <cheekhuin.loong@gmail.com>
2018-12-09 17:05:40 +0800

    created constants file for TIB related constants
    created a timedscript for testing

---
+++ server/helper/server_info/tib.ts
@@ -0,0 +1,5 @@
+export const TIBID = "495609306436075530";
+export const welcomeID = "517033870785511425";
+export const rulesID = "517061735639678977";
+export const announcementsID = "512661992255782913";
+export const tourneyRulesID = "517062872056987668";
